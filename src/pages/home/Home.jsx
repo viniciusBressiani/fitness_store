01a4200427ddba843e3de1e40083ece0004439d3
viniciusBressiani <88894499+viniciusBressiani@users.noreply.github.com>
2022-12-04 09:13:54 -0300

    Finished the programs of home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import MainHeader from '../../components/MainHeader';
+import Programs from '../../components/Programs';
 import './home.css';
 
 const Home = () => {
   return (
     <div>
       <MainHeader />
+      <Programs />
       Home
     </div>
   )
