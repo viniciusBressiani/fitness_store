ce09352d9a70c147b6c00fed869690d65938bf45
viniciusBressiani <88894499+viniciusBressiani@users.noreply.github.com>
2022-12-07 15:22:25 -0300

    Finished Gallery with jsx and css

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import headerImage from '../../images/header_bg_3.jpg'
+
 import './gallery.css'
+import Header from './../../components/Header';
 
 const Gallery = () => {
+  const galleryLength = 15;
+  const images = [];
+
+  for(let i = 1; i <= galleryLength; i++) {
+    images.push(require(`../../images/gallery${i}.jpg`))
+  }
+
   return (
-    <div>
-      Gallery
-    </div>
+    <>
+    <Header title='Our Gallery' image={headerImage}>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo saepe necessitatibus error sunt, rerum quibusdam repudiandae voluptas animi eaque blanditiis.</p>
+    </Header>
+    <section className="gallery">
+      <div className="container gallery__container">
+        {
+          images.map((image, index) => {
+            return <article key={index}>
+              <img src={image} alt={`Gallery Image ${index + 1}`} />
+            </article>
+          })
+        }
+      </div>
+    </section>
+    </> 
   )
 }
 
