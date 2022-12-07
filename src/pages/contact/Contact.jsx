a98e64db531bbc3977477985e4b89ff9f0516685
viniciusBressiani <88894499+viniciusBressiani@users.noreply.github.com>
2022-12-07 09:46:53 -0300

    Finished Contact Page with jsx and css

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import headerImage from '../../images/header_bg_2.jpg';
+import {MdEmail} from 'react-icons/md';
+import {BsMessenger} from 'react-icons/bs';
+import {IoLogoWhatsapp} from 'react-icons/io';
+
+
 import './contact.css';
+import Header from './../../components/Header';
 
 const Contact = () => {
   return (
-    <div>
-      Contact
-    </div>
+    <>
+    <Header image={headerImage} title="Get in Touch">
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius, eum? Obcaecati impedit tenetur ratione totam doloremque nemo itaque error quam.</p>
+    </Header>
+    <section className="contact">
+      <div className="container contact__container">
+        <div className="contact__wrapper">
+          <a href="mailto:caio@caio.com" target="_black" rel="noreferrer noopener"><MdEmail/></a>
+          <a href="http://m.me/facebook_name" target="_black" rel="noreferrer noopener"><BsMessenger /></a>
+          <a href="https://wa.me/+554788888888" target="_black" rel="noreferrer noopener"><IoLogoWhatsapp /></a>
+        </div>
+      </div>
+    </section>
+    </>
   )
 }
 
