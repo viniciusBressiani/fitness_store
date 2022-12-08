367c5b94ab12508e1d737877eb41dad7cc8e434a
viniciusBressiani <88894499+viniciusBressiani@users.noreply.github.com>
2022-12-08 08:12:50 -0300

    Finished NotFound with jsx and css

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 import './notfound.css';
 
 const NotFound = () => {
   return (
-    <div>
-      NotFound
-    </div>
+    <section>
+      <div className="container notfound__container">
+        <h2>Page Not Found</h2>
+        <Link to="/" className='btn'>Go Back Home</Link>
+      </div>
+    </section>
   )
 }
 
