0c29ab39d9d0a097c533699317a6b0518538450f
viniciusBressiani <88894499+viniciusBressiani@users.noreply.github.com>
2022-12-08 09:10:06 -0300

    Finished Plans with jsx and css

--- src/pages/plans/Plans.jsx
+++ src/pages/plans/Plans.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import Header from './../../components/Header';
+import Card from './../../UI/Card';
+
+import headerImage from '../../images/header_bg_4.jpg'
+import {plans} from '../../data'
+
 import './plans.css';
 
 const Plans = () => {
   return (
-    <div>
-      Plans
-    </div>
+   <>
+    <Header title='Membership Plans' image={headerImage}>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi mollitia sequi, sed sunt libero iste.</p>
+    </Header>
+    <section className="plans">
+      <div className="container plans__container">
+        {
+          plans.map(({id, name, desc, price, features})=> {
+            return <Card key={id} className='plan'>
+               <h3>{name}</h3>
+               <small>{desc}</small>
+               <h1>{`$${price}`}</h1><h2>/mo</h2>
+               <h4>Features</h4>
+               {
+                features.map(({feature,available}, index) => {
+                  return <p key={index}
+                    className={!available ? 'disabled': ''}>
+                    {feature}
+                  </p>
+                })
+               }
+               <button className='btn lg'>Choose Plan</button> 
+            </Card>
+          })
+        }
+      </div>
+    </section>
+   </>
   )
 }
 
